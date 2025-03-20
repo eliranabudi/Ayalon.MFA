99b69766299880ab5663be7f175cf312fb5f370e
eliranabudi <eliranabudi@gmail.com>
2025-03-20 16:02:39 +0200

    Add files via upload

---
+++ Ayalon.MFA.Core/QrCodeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using QRCoder;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Ayalon.MFA.Core
+{
+    public class QrCodeGenerator
+    {
+        public Bitmap GenerateQrCode(string secretKey, string user, string issuer)
+        {
+            var totpUri = $"otpauth://totp/{issuer}:{user}?secret={secretKey}&issuer={issuer}&digits=6";
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(totpUri, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            return qrCode.GetGraphic(4);
+        }
+        
+        public BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Bmp);
+                ms.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = ms;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+    }
+}
---
+++ Ayalon.MFA.Core/TwoFactorAuth.cs
@@ -0,0 +1,75 @@
+﻿using OtpNet;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Ayalon.MFA.Core
+{
+    public class TwoFactorAuth
+    {
+        private string secretKey;
+
+        public TwoFactorAuth()
+        {
+            secretKey = GenerateSecretKey();
+        }
+
+        public string GenerateCode()
+        {
+            var totp = new Totp(Base32Encoding.ToBytes(secretKey));
+            return totp.ComputeTotp();
+        }
+
+        public bool ValidateCode(string code)
+        {
+            var totp = new Totp(Base32Encoding.ToBytes(secretKey));
+            return totp.VerifyTotp(code, out long timeStepMatched, new VerificationWindow(0, 0));
+        }
+
+        public string GenerateSecretKey()
+        {
+            byte[] key = new byte[20];
+            RandomNumberGenerator.Fill(key);
+            return Base32Encoding.ToString(key);
+        }
+
+        //public string EncryptSecretKey(string secretKey)
+        //{
+        //    return Convert.ToBase64String(Encoding.UTF8.GetBytes(secretKey));
+        //}
+
+        //public string DecryptSecretKey(string encryptedSecretKey)
+        //{
+        //    return Encoding.UTF8.GetString(Convert.FromBase64String(encryptedSecretKey));
+        //}
+
+        public string EncryptSecretKey(string secretKey)
+        {
+            if (string.IsNullOrEmpty(secretKey))
+            {
+                throw new Exception("The secret key cannot be null or empty!");
+            }
+
+            byte[] dataBytes = Encoding.UTF8.GetBytes(secretKey);
+            byte[] encryptedData = ProtectedData.Protect(dataBytes, null, DataProtectionScope.CurrentUser);
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        public string DecryptSecretKey(string encryptedSecretKey)
+        {
+            if (string.IsNullOrEmpty(encryptedSecretKey))
+            {
+                throw new Exception("The encrypted secret key cannot be null or empty!");
+            }
+
+            byte[] encryptedData = Convert.FromBase64String(encryptedSecretKey);
+            byte[] decryptedData = ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.CurrentUser);
+            return Encoding.UTF8.GetString(decryptedData);
+        }
+
+        public void SetSecretKey(string base32Secret)
+        {
+            secretKey = base32Secret;
+        }
+    }
+}
