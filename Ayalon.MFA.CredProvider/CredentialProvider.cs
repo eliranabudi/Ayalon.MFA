eb5e72ac5fee6efcc8e1705fbe78e22677f411ff
eliranabudi <eliranabudi@gmail.com>
2025-03-20 16:04:27 +0200

    Add files via upload

--- Ayalon.MFA.CredProvider/CredentialProvider.cs
+++ Ayalon.MFA.CredProvider/CredentialProvider.cs
@@ -1,4 +1,4 @@
-using Ayalon.MFA.Core;
+﻿using Ayalon.MFA.Core;
 using CredProvider.NET.Interop2;
 using System;
 using System.Drawing;
---
+++ Ayalon.MFA.CredProvider/certificate creation.ps1
@@ -0,0 +1,8 @@
+﻿$selfSignedRootCA = New-SelfSignedCertificate -type CodeSigningCert -DnsName "Ayalon.MFA.CredProvider" -notafter (Get-Date).AddMonths(12) -CertStoreLocation Cert:\LocalMachine\My\ -KeyExportPolicy Exportable -KeyUsage CertSign,CRLSign,DigitalSignature -KeySpec KeyExchange -KeyLength 2048 -KeyUsageProperty All -KeyAlgorithm 'RSA' -HashAlgorithm 'SHA256' -Provider 'Microsoft Enhanced RSA and AES Cryptographic Provider'
+$CertPassword = ConvertTo-SecureString -String "MyPassword" -Force -AsPlainText
+$selfSignedRootCA | Export-PfxCertificate -FilePath C:\Ayalon.MFA.CredProvider.pfx -Password $CertPassword
+
+Import-PfxCertificate -FilePath "C:\Ayalon.MFA.CredProvider.pfx" -CertStoreLocation "Cert:\LocalMachine\My" -Password $CertPassword
+
+
+#signtool sign /fd SHA256 /f "C:\Ayalon.MFA-CredProvider.pfx" /p "MyPassword" /td SHA256 /v "C:\Windows\System32\AyalonMFA\Ayalon.MFA.CredProvider.dll"
